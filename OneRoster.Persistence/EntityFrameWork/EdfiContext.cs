488e0749089af2e1dbdf6607069b2dd4069e1ea8
Doug Loyo <douglasloyo@gmail.com>
2021-06-08 17:51:07 -0500

    Cleaning up the EdFiContext.

--- OneRoster.Persistence/EntityFrameWork/EdfiContext.cs
+++ OneRoster.Persistence/EntityFrameWork/EdfiContext.cs
@@ -21,73 +21,53 @@ namespace EdFi.OneRoster.Persistence.EntityFrameWork
             modelBuilder.Entity<academicsessions>(entity =>
             {
                 entity.HasNoKey();
-
                 entity.ToTable("academicsessions", "onerosterv11");
-
                 //entity.Property(e => e.title).HasColumnType("character varying");
             });
 
             modelBuilder.Entity<classes>(entity =>
             {
                 entity.HasNoKey();
-
                 entity.ToTable("classes", "onerosterv11");
 
                 //entity.Property(e => e.dateLastModified).HasColumnType("timestamp with time zone");
-
-                entity.Property(e => e.location).HasMaxLength(60);
+                //entity.Property(e => e.location).HasMaxLength(60);
             });
 
             modelBuilder.Entity<courses>(entity =>
             {
                 entity.HasNoKey();
-
                 entity.ToTable("courses", "onerosterv11");
 
-                entity.Property(e => e.courseCode).HasMaxLength(60);
-
+                //entity.Property(e => e.courseCode).HasMaxLength(60);
                 //entity.Property(e => e.dateLastModified).HasColumnType("timestamp with time zone");
-
-                entity.Property(e => e.title).HasMaxLength(60);
+                //entity.Property(e => e.title).HasMaxLength(60);
             });
 
             modelBuilder.Entity<demographics>(entity =>
             {
                 entity.HasNoKey();
-
                 entity.ToTable("demographics", "onerosterv11");
 
                 //entity.Property(e => e.americanIndianOrAlaskaNative).HasColumnType("character varying");
-
                 //entity.Property(e => e.asian).HasColumnType("character varying");
-
                 //entity.Property(e => e.birthDate).HasColumnType("date");
-
                 //entity.Property(e => e.blackOrAfricanAmerican).HasColumnType("character varying");
-
                 //entity.Property(e => e.dateLastModified).HasColumnType("timestamp with time zone");
-
                 //entity.Property(e => e.demographicRaceTwoOrMoreRaces).HasColumnType("character varying");
-
                 //entity.Property(e => e.hispanicOrLatinoEthnicity).HasColumnType("character varying");
-
                 //entity.Property(e => e.nativeHawaiianOrOtherPacificIslander).HasColumnType("character varying");
-
                 //entity.Property(e => e.white).HasColumnType("character varying");
             });
 
             modelBuilder.Entity<enrollments>(entity =>
             {
                 entity.HasNoKey();
-
                 entity.ToTable("enrollments", "onerosterv11");
-
                 entity.Property(e => e._class).HasColumnName("class");
 
                 //entity.Property(e => e.beginDate).HasColumnType("timestamp with time zone");
-
                 //entity.Property(e => e.dateLastModified).HasColumnType("timestamp with time zone");
-
                 //entity.Property(e => e.endDate).HasColumnType("timestamp with time zone");
             });
 
@@ -100,21 +80,13 @@ namespace EdFi.OneRoster.Persistence.EntityFrameWork
             modelBuilder.Entity<users>(entity =>
             {
                 entity.HasNoKey();
-
                 entity.ToTable("users", "onerosterv11");
-
-                entity.Property(e => e.familyName).HasMaxLength(75);
-
-                entity.Property(e => e.givenName).HasMaxLength(75);
-
-                entity.Property(e => e.identifier).HasMaxLength(32);
-
+                //entity.Property(e => e.familyName).HasMaxLength(75);
+                //entity.Property(e => e.givenName).HasMaxLength(75);
+                //entity.Property(e => e.identifier).HasMaxLength(32);
                 //entity.Property(e => e.dateLastModified).HasColumnType("timestamp with time zone");
-
                 //entity.Property(e => e.email).HasColumnType("character varying");
-
                 //entity.Property(e => e.middleName).HasColumnType("character varying");
-
                 //entity.Property(e => e.username).HasColumnType("character varying");
             });
         }
